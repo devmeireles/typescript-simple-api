f4839c7ec7d57ba7a86cee9d74a7ef259a276c03
Gabriel Meireles <dev.meireles@gmail.com>
2021-04-12 18:26:51 -0300

    Updating groups of test

--- test/e2e/product.test.ts
+++ test/e2e/product.test.ts
@@ -32,32 +32,32 @@ describe('Testing the product endpoints', () => {
         price: 10.99
     }
 
-    it('It should POST an user', async () => {
-        const { body, status } = await global.testRequest.post('/user').send(userObj);
-        expect(status).toBe(201);
-        expect(body.success).toEqual(true);
-        expect(body.data).toHaveProperty('name');
-        expect(body.data).toHaveProperty('email');
-
-        userID = body.data.id;
-        storeObj.owner_id = userID;
-        productObj.owner_id = userID;
-    });
-
-    it('It should do the login action and return a jwt', async () => {
-        const { body, status } = await global.testRequest.post('/auth/login').send(loginObj);
-        expect(status).toBe(200);
-        expect(body.success).toEqual(true);
-        expect(body.data).toHaveProperty('name');
-        expect(body.data).toHaveProperty('email');
-        expect(body.data).toHaveProperty('token');
-
-        token = body.data.token;
-    });
-
     describe('POST /', () => {
+        it('It should POST an user', async () => {
+            const { body, status } = await global.testRequest.post('/user').send(userObj);
+            expect(status).toBe(201);
+            expect(body.success).toEqual(true);
+            expect(body.data).toHaveProperty('name');
+            expect(body.data).toHaveProperty('email');
+
+            userID = body.data.id;
+            storeObj.owner_id = userID;
+            productObj.owner_id = userID;
+        });
+
+        it('It should do the login action and return a jwt', async () => {
+            const { body, status } = await global.testRequest.post('/auth/login').send(loginObj);
+            expect(status).toBe(200);
+            expect(body.success).toEqual(true);
+            expect(body.data).toHaveProperty('name');
+            expect(body.data).toHaveProperty('email');
+            expect(body.data).toHaveProperty('token');
+
+            token = body.data.token;
+        });
+
         it('It should POST a store', async () => {
-            const { body, status } = await global.testRequest.post('/store').send(storeObj).set({'authorization': token});
+            const { body, status } = await global.testRequest.post('/store').send(storeObj).set({ 'authorization': token });
             expect(status).toBe(201);
             expect(body.success).toEqual(true);
             expect(body.data).toHaveProperty('name');
@@ -66,9 +66,7 @@ describe('Testing the product endpoints', () => {
 
             productObj.store_id = body.data.id;
         });
-    });
 
-    describe('POST /', () => {
         it('It should POST a product', async () => {
             const { body, status } = await global.testRequest.post('/product').send(productObj).set({ 'authorization': token });
             expect(status).toBe(201);
@@ -77,11 +75,9 @@ describe('Testing the product endpoints', () => {
             expect(body.data).toHaveProperty('id');
             expect(body.data).toHaveProperty('description');
         });
-    });
 
-    describe('POST /', () => {
         it("It shouldn't POST a product because the authorization header is wrong", async () => {
-            const { body, status } = await global.testRequest.post('/product').send(productObj).set({'authorization': wrongToken});
+            const { body, status } = await global.testRequest.post('/product').send(productObj).set({ 'authorization': wrongToken });
             expect(status).toBe(401);
             expect(body.success).toEqual(false);
             expect(body).not.toHaveProperty('data');
--- test/e2e/store.test.ts
+++ test/e2e/store.test.ts
@@ -33,69 +33,62 @@ describe('Testing the store endpoints', () => {
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
     }
 
-    it('It should POST an user', async () => {
-        const { body, status } = await global.testRequest.post('/user').send(userObj);
-        expect(status).toBe(201);
-        expect(body.success).toEqual(true);
-        expect(body.data).toHaveProperty('name');
-        expect(body.data).toHaveProperty('email');
-
-        userID = body.data.id;
-        storeObj.owner_id = userID;
-    });
-
-    it('It should do the login action and return a jwt', async () => {
-        const { body, status } = await global.testRequest.post('/auth/login').send(loginObj);
-        expect(status).toBe(200);
-        expect(body.success).toEqual(true);
-        expect(body.data).toHaveProperty('name');
-        expect(body.data).toHaveProperty('email');
-        expect(body.data).toHaveProperty('token');
-
-        token = body.data.token;
-    });
-
     describe('POST /', () => {
+        it('It should POST an user', async () => {
+            const { body, status } = await global.testRequest.post('/user').send(userObj);
+            expect(status).toBe(201);
+            expect(body.success).toEqual(true);
+            expect(body.data).toHaveProperty('name');
+            expect(body.data).toHaveProperty('email');
+
+            userID = body.data.id;
+            storeObj.owner_id = userID;
+        });
+
+        it('It should do the login action and return a jwt', async () => {
+            const { body, status } = await global.testRequest.post('/auth/login').send(loginObj);
+            expect(status).toBe(200);
+            expect(body.success).toEqual(true);
+            expect(body.data).toHaveProperty('name');
+            expect(body.data).toHaveProperty('email');
+            expect(body.data).toHaveProperty('token');
+
+            token = body.data.token;
+        });
+
         it('It should POST a store', async () => {
-            const { body, status } = await global.testRequest.post('/store').send(storeObj).set({'authorization': token});
+            const { body, status } = await global.testRequest.post('/store').send(storeObj).set({ 'authorization': token });
             expect(status).toBe(201);
             expect(body.success).toEqual(true);
             expect(body.data).toHaveProperty('name');
             expect(body.data).toHaveProperty('id');
             expect(body.data).toHaveProperty('description');
         });
-    });
 
-    describe('POST /', () => {
+
         it("It shouldn't POST a store because the body is empty", async () => {
-            const { body, status } = await global.testRequest.post('/store').send({}).set({'authorization': token});
+            const { body, status } = await global.testRequest.post('/store').send({}).set({ 'authorization': token });
             expect(status).toBe(422);
             expect(body.success).toEqual(false);
             expect(body).not.toHaveProperty('data');
         });
-    });
 
-    describe('POST /', () => {
         it("It shouldn't POST a store because the user doesn't exist", async () => {
-            const { body, status } = await global.testRequest.post('/store').send(wrongStoreUserObj).set({'authorization': token});
+            const { body, status } = await global.testRequest.post('/store').send(wrongStoreUserObj).set({ 'authorization': token });
             expect(status).toBe(400);
             expect(body.success).toEqual(false);
             expect(body).not.toHaveProperty('data');
         });
-    });
 
-    describe('POST /', () => {
         it("It shouldn't POST a store because the authorization header is missing", async () => {
             const { body, status } = await global.testRequest.post('/store').send(storeObj);
             expect(status).toBe(401);
             expect(body.success).toEqual(false);
             expect(body).not.toHaveProperty('data');
         });
-    });
 
-    describe('POST /', () => {
         it("It shouldn't POST a store because the authorization header is wrong", async () => {
-            const { body, status } = await global.testRequest.post('/store').send(storeObj).set({'authorization': wrongToken});
+            const { body, status } = await global.testRequest.post('/store').send(storeObj).set({ 'authorization': wrongToken });
             expect(status).toBe(401);
             expect(body.success).toEqual(false);
             expect(body).not.toHaveProperty('data');
